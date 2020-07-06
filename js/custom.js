b2dcade54915b0a3e935fc8bfcd6b98266163709
unknown <kasra.parhun@gmail.com>
2020-07-06 22:21:54 +0430

    ptoject completed

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+const controls = document.querySelectorAll(".controls input");
+
+function handleUpdate(){
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+controls.forEach(control => control.addEventListener("change", handleUpdate));
+controls.forEach(control => control.addEventListener("mousemove", handleUpdate));
+// controls.foreach(function(control){
+//   control.addEventListener("onchange", function(evt){
+//     // evt.target.style.
+//   })
+// })
